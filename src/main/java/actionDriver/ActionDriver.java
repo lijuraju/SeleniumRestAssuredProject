f957ee184da2e4fdc8575bbc8f76aadb61a29621
lijuraju <liju.raju@philips.com>
2025-02-26 21:38:55 +0530

    Second commit - ActionDriver and Base Class

--- src/main/java/actionDriver/ActionDriver.java
+++ src/main/java/actionDriver/ActionDriver.java
@@ -1,4 +1,54 @@
 package actionDriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ActionDriver {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public ActionDriver(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void waitForElementToBeClickable(By by){
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public void waitForElementToBeVisible(By by){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public void click(By by){
+        driver.findElement(by).click();
+    }
+
+    public void enterText(By by,String text){
+        driver.findElement(by).sendKeys(text);
+    }
+
+    public String getText(By by){
+        waitForElementToBeVisible(by);
+       return driver.findElement(by).getText();
+    }
+
+    public boolean isDisplayed(By by){
+
+        waitForElementToBeVisible(by);
+        return driver.findElement(by).isDisplayed();
+    }
+
+    public void scrollToElement(By by){
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(by));
+    }
 }
--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -6,58 +6,75 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 public class BaseClass {
 
-    protected Properties prop;
-    protected WebDriver driver;
+    protected static Properties prop;
+    protected static WebDriver driver;
 
     @BeforeMethod
     public void setUp() throws IOException {
+        System.out.println("Setting up the test" + this.getClass().getSimpleName());
+        launchBrowser();
+        configureBrowser();
+        staticWait(2);
+    }
+
+    @BeforeSuite
+    public void loadConfig() throws IOException {
         prop = new Properties();
         FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
         prop.load(fis);
+    }
 
-        //Intialize the driver based on browser defined in config.properties
-
+    private void launchBrowser() {
         String browser = prop.getProperty("browser");
 
-        if(browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("Invalid Browser");
         }
 
-        else if (browser.equalsIgnoreCase("firefox")) {
-            //Initialize the driver for firefox
-            driver = new FirefoxDriver();
-        }
+    }
 
-        else if (browser.equalsIgnoreCase("edge")) {
-           driver = new EdgeDriver();
-        }
-        else {
-            System.out.println("Invalid browser");
-        }
-
-        //Implicit wait
+    //Configure browser settings
+    private void configureBrowser() {
         int implicitWait = Integer.parseInt(prop.getProperty("implicitWait"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
 
-        //maximize the browser
         driver.manage().window().maximize();
-
-        //navigate to URL
         driver.get(prop.getProperty("url"));
 
+
     }
+
 
     @AfterMethod
     public void tearDown() {
         driver.quit();
+    }
+
+    public void staticWait(int seconds){
+
+        LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(seconds));
     }
 
 
